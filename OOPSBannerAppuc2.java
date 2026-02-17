08f98289af8aec2f33cac6efd4a34a4fd0b2ea92
theabdlrah <abdulrahmanibnfarees@gmail.com>
2026-02-17 09:18:22 +0530

    Add files via upload

---
+++ OOPSBannerAppuc2.java
@@ -0,0 +1,21 @@
+/**
+ * OOPS Banner Application - UC2
+ * Displays "OOPS" in large ASCII banner format.
+ *
+ * @author guru14114
+ * @version 2.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *****   *****  ");
+        System.out.println("*     * *     * *        *     ");
+        System.out.println("*     * *     * *        *     ");
+        System.out.println(" *****   *****   *        ***** ");
+    }
+}
